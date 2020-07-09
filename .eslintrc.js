e7a573ba5341d103e0dca2ef3a5c11fd6b926eab
martinholden-skillsoft <12408585+martinholden-skillsoft@users.noreply.github.com>
2020-07-09 09:32:41 +0100

    Updated packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
   env: {
     es6: true,
-    node: true
+    node: true,
   },
   rules: {
     'max-len': [
@@ -16,8 +16,8 @@ module.exports = {
         ignoreUrls: true,
         ignoreStrings: true,
         ignoreTemplateLiterals: true,
-        ignoreRegExpLiterals: true
-      }
-    ]
-  }
+        ignoreRegExpLiterals: true,
+      },
+    ],
+  },
 };
